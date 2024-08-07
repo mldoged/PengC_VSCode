cad6deeafe4d33f61ae1965be1b2f7a93a1da6bb
huangshun <1692470435@qq.com>
2024-08-07 08:25:38 +0800

    end of c

---
+++ Contact_Dynamic_file/test.c
@@ -0,0 +1,57 @@
+
+#include"contact.h"
+void menu()
+{
+    printf("##############################\n");
+    printf("##  1、add        2、del    ##\n");
+    printf("##  3、search     4、modify ##\n");
+    printf("##  5、show       6、sort   ##\n");
+    printf("##  7、save       0、exit   ##\n");
+    printf("##############################\n");
+}
+int main()
+{
+    struct Contact con;//创建通讯录
+    int input = 1;
+    InitContact(&con);//初始化数组
+    do
+    {
+        menu();
+        printf("请选择->");
+        scanf("%d",&input);
+        switch (input)
+        {
+        case ADD:
+            AddContact(&con);//增加
+            break;
+        case DEL:
+            DelContact(&con);//删除
+            break;
+        case SERACH:
+            SearchContact(&con);//查找并显示
+            break;
+        case MODIFY:
+            ModifyContact(&con);
+            break;
+        case SHOW:
+            ShowContact(&con);//显示所有成员
+            break;
+        case SORT:
+            printf("sort\n");
+            break;
+        case SAVE:
+            SaveContact(&con);
+            break;
+        case EXIT:
+            SaveContact(&con);
+            DestroyContact(&con);
+            printf("退出通讯录\n");
+            break;
+        default:
+            printf("选择错误！\n");
+            break;
+        }
+    } while (input);
+    
+    return 0;
+}
--- P31-P84/P71-84.c
+++ P31-P84/P71-84.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
+#define do_forever for(;;)
+#define SQUARE(x) ((x)*(x))
+#define PRINT(x) printf("the value of"#x"is %d\n",x)
+#define CAT(x,y) x##y
+#define MALLOC(num,type) (type*)malloc(num*sizeof(type))
+
 int main()
 {
-    int i = 10000;
+    /* int i = 10000;
     FILE* pf = fopen("test.txt","wb");
     fwrite(&i,4,1,pf);
     fclose(pf);
-    pf = NULL;
+    pf = NULL; */
+
+    printf("%d",__STDC__);
     return 0;
 }
